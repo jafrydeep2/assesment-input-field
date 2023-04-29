df3637fe55a5de704a642bc6cc8102868d4059da
Jafry Mondol Deep <jafrybd42@gmail.com>
2023-04-29 17:34:53 +0600

    work in progress

---
+++ src/pages/home/dummyData.ts
@@ -0,0 +1,7 @@
+export const dummyList = [
+  { name: 'Data 1', sector: 10, agree: true, hasAccess: false },
+  { name: 'Data 2', sector: 40, agree: true, hasAccess: false },
+  { name: 'Data 3', sector: 60, agree: true, hasAccess: false },
+  { name: 'Data 4', sector: 42, agree: true, hasAccess: false },
+  { name: 'Data 5', sector: 35, agree: true, hasAccess: false },
+];
